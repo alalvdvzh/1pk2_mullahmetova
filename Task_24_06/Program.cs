b20725dfa84c916cb1f6c4ec9a906a314c0f98e8
shlepske14 <shlepske14@xmail.ru>
2025-04-15 09:35:01 +0500

    задание 6 урока 24 выполнено

---
+++ Task_24_06/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_24_06
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {/*Напишите метод, который принимает путь к файлу и возвращает количество строк в нем. ИспользуйтеStreamReader.*/
+        static void Main()
+        {
+            //  Путь к файлу
+            string filePath = @"C:\test\example.txt";
+            int lineCount = CountLinesInFile(filePath);
+            Console.WriteLine($"The file has {lineCount} line(s)");
+            Console.ReadLine();
+        }
+
+        // Метод для подсчёта строк в файле
+        static int CountLinesInFile(string path)
+        {
+            int counter = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
