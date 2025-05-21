5493ffcd74fd7345852789d7462a30fc08a78bef
shlepske14 <shlepske14@xmail.ru>
2025-05-21 09:54:06 +0500

    выполнено задание 2

---
+++ Task_39_02/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_39_02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> Products { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Products = new ObservableCollection<string>();
+            DataContext = this;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ProductTextBox.Text))
+            {
+                Products.Add(ProductTextBox.Text);
+                ProductTextBox.Clear();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProductsListBox.SelectedItem != null)
+            {
+                Products.Remove(ProductsListBox.SelectedItem as string);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt",
+                DefaultExt = "txt",
+                FileName = "Список покупок.txt"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllLines(saveFileDialog.FileName, Products);
+                MessageBox.Show("Список сохранен успешно!", "Успех",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
