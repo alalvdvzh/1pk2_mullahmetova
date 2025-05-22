0f76449bd2ed00ce0aefd664474f6914ddc855c3
shlepske14 <shlepske14@xmail.ru>
2025-05-22 10:42:53 +0500

    задание 6 пары 10 выполнено

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_10_06
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            ArrayGeneration(5); // Пример вызова метода с n = 5
+        }
+
+        // Метод генерирует и выводит массив n x n
+        public static void ArrayGeneration(int n)
+        {
+            int[,] array = new int[n, n];
+            Random rand = new Random();
+
+            // Заполнение массива случайными числами от 1 до 100
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = rand.Next(1, 101);
+                }
+            }
+
+            // Вывод массива на консоль
+            Console.WriteLine($"Массив {n}xn:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j].ToString().PadLeft(4) + " "); // Форматирование вывода
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
