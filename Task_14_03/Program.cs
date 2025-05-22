c3c2530240df9bb8f1c06aa59c1040e945e248d9
shlepske14 <shlepske14@xmail.ru>
2025-05-22 11:32:25 +0500

    задание 3 урока 14 выполнено

---
+++ Task_14_03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_14_03
+{
+    using System;
+
+    public static class MathOperations
+    { //Реализуйте статический метод Factorial, который принимает целое число и возвращает его факториал.Сделайтетак, чтобы метод работал только для не отрицательных чисел.
+        public static int Factorial(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Число не может быть отрицательным.");
+
+            if (n == 0)
+                return 1;
+
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
