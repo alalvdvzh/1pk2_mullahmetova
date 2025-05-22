254ecfeabad28b23f108ba9728bae78912281785
shlepske14 <shlepske14@xmail.ru>
2025-05-22 11:39:17 +0500

    задание 4 урока 14 выполнено

---
+++ Task_14_04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_14_04
+{
+    public class User
+    {//Определите класс User, который будет иметь статическое свойство Current User, представляющее текущего пользователя, и метод для его установки   
+        // Статическое свойство для хранения текущего пользователя
+        public static User CurrentUser { get; private set; }
+
+        // Обычные свойства пользователя
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        // Статический метод для установки текущего пользователя
+        public static void SetCurrentUser(User user)
+        {
+            CurrentUser = user;
+        }
+
+        // Конструктор для удобного создания объектов User
+        public User(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
